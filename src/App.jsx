b6b37d3071cdbcfe519bb403f984662f5ba12853
mirAIlY-peek <miras.kural@mail.ru>
2024-07-09 17:27:39 +0500

    update vercel analitycs

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Lending from "./pages/Lending.jsx";
 import EmoAI from "./pages/EmoAI.jsx";
+import { Analytics } from "@vercel/analytics/react"
+// import {SpeedInsights} from "@vercel/speed-insights/dist/react/index.js";
 // import Registration from "./pages/Registration.jsx";
 // import Search from "./pages/Search.jsx";
 
 const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Lending />} />
-            <Route path="/pricing" element={<EmoAI />} />
-
-            {/*<Route path="/search" element={<Search />} />*/}
-            {/*<Route path="/registration" element={<Registration />} />*/}
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<Lending />} />
+                <Route path="/pricing" element={<EmoAI />} />
+            </Routes>
+            <Analytics />
+        </>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
-        <SpeedInsights />
     </Router>
   </React.StrictMode>
 );
