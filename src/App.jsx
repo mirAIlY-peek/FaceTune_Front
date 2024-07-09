a160f183187aeaf996c693db959991080c8b134b
mirAIlY-peek <miras.kural@mail.ru>
2024-07-09 17:16:05 +0500

    moved vercel to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Lending from "./pages/Lending.jsx";
 import EmoAI from "./pages/EmoAI.jsx";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 // import Registration from "./pages/Registration.jsx";
 // import Search from "./pages/Search.jsx";
 
@@ -11,7 +10,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={<Lending />} />
             <Route path="/pricing" element={<EmoAI />} />
-            <SpeedInsights />
+
             {/*<Route path="/search" element={<Search />} />*/}
             {/*<Route path="/registration" element={<Registration />} />*/}
         </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <App />
+        <SpeedInsights />
     </Router>
   </React.StrictMode>
 );
