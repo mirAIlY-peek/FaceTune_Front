3a1d6add833982d26ff209680b08fc07b23aa093
mirAIlY-peek <miras.kural@mail.ru>
2024-07-22 22:41:02 +0500

    some upd

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Lending from "./pages/Lending.jsx";
 import EmoAI from "./pages/EmoAI.jsx";
 import Spotify from "./pages/Spotify.jsx";
-
 import { Analytics } from "@vercel/analytics/react"
-// import {SpeedInsights} from "@vercel/speed-insights/dist/react/index.js";
-// import Registration from "./pages/Registration.jsx";
-// import Search from "./pages/Search.jsx";
+
 
 const App = () => {
 
